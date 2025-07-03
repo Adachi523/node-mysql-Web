4fb68cdb1846138c8b0aa0af5dc275d43da38931
あだ <j2202@matsue-ct.ac.jp>
2025-07-03 05:38:58 +0000

    chap15

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 const mysql = require('mysql');
-
-let todos = [];
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,31 +10,37 @@ const connection = mysql.createConnection({
   database: 'todo_app'
 });
 
-let todos = [];
-
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 router.post('/', function (req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
   const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 module.exports = router;
